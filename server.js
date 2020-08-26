3cacf8b7c9cc4fd69c5a2fdd4b7e9225b9c7efbc
Matt Huntington <matt.huntington@gmail.com>
2020-08-26 11:27:58 -0400

    adding pg

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Client = require('pg').Client
 const app = express();
 
 app.use(express.json());
@@ -6,6 +7,18 @@ app.use(express.static('public'))
 
 const peopleController = require('./controllers/people.js');
 app.use('/people', peopleController);
+
+const client = new Client({
+    user: '',
+    host: 'localhost',
+    database: 'contacts',
+    password: '',
+    port: 5432,
+})
+client.connect();
+client.query('SELECT * FROM people;', (err, res) => {
+    console.log(res.rows)
+})
 
 app.listen(3000, () => {
     console.log('listening');
