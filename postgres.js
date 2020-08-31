fd200cc35b21cf922937a2ce0cf116eafd69c48a
Matt Huntington <matt.huntington@gmail.com>
2020-08-30 20:28:33 -0400

    adjust postgres for heroku

--- postgres.js
+++ postgres.js
@@ -1,11 +1,7 @@
 const Client = require('pg').Client
 
 const client = new Client({
-    user: '',
-    host: 'localhost',
-    database: 'contacts',
-    password: '',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/contacts',
 })
 
 module.exports = client;
